9180254ac9802d3ce73872e8e8e82ea2a5d59ecd
Salsabila Fatma Aripa <114417418+bilaaripa@users.noreply.github.com>
2023-11-01 09:30:43 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Kalkulator Sederhana</h1>
+    <p>Hasil: {{ hasil }}</p>
+    <input v-model="angka1" type="number" placeholder="Angka pertama" />
+    <input v-model="angka2" type="number" placeholder="Angka kedua" />
+    <br /><br />
+    <button @click="add">+</button>
+    <button @click="subtract">-</button>
+    <button @click="multiply">*</button>
+    <button @click="divide">/</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      angka1: 0,
+      angka2: 0,
+      hasil: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.hasil = this.angka1 + this.angka2;
+    },
+    subtract() {
+      this.hasil = this.angka1 - this.angka2;
+    },
+    multiply() {
+      this.hasil = this.angka1 * this.angka2;
+    },
+    divide() {
+      this.hasil = this.angka1 / this.angka2;
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+/* Add scoped styles here if needed */
 </style>
